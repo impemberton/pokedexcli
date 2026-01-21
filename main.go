9d946f2c0e0dab51e98ddde6837d23230b0ff5d4
iketyke <isaac.mellad.pemberton@gmail.com>
2026-01-21 19:28:42 +0000

    implement exit and help command

--- main.go
+++ main.go
@@ -5,14 +5,65 @@ import (
 	"bufio"
 	"os"
 )
+func init() {
+registry = map[string]cliCommand {
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+}
+}
+
+type cliCommand struct {
+		name string
+		description string
+		callback func() error
+	}
+var registry map[string]cliCommand
+
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")	
+	fmt.Println("Usage:\n")	
+	for _, command := range registry {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
+
 
 func main() {
+
+	
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		user_input := cleanInput(scanner.Text())
-		fmt.Println("Your command was: " + user_input[0])
+		found := false
+		for _, command := range registry {
+			if user_input[0] == command.name {
+				found = true
+				command.callback()
+			}
+		}
+		if !found {
+			fmt.Println("Unknown command")
+		}
 	}
 }
 
